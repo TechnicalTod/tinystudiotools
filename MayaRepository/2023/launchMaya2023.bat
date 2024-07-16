4618880da9d13df34d012a227614c3f3d7120a79
werksone <werksone@gmail.com>
2024-07-16 20:07:03 +1200

    update

--- MayaRepository/2023/launchMaya2023.bat
+++ MayaRepository/2023/launchMaya2023.bat
@@ -2,11 +2,15 @@
 
 REM Set Maya version and script directory
 set MAYAVERSION=2023
+REM Set the folder that this Bat file is being run from
 set SCRIPT_DIR=%~dp0
 
 REM Set Maya application paths
 set MAYA_APP_BASE_PATH=C:\Program Files\Autodesk\Maya%MAYAVERSION%
 set MAYA_APP_PATH=%MAYA_APP_BASE_PATH%\bin\maya.exe
+
+REM Set Show Directory
+set SAGA_SHOW_DIR=S:\
 
 REM Set custom script paths
 set SAGA_MAYA_SCRIPT_PATH=%SCRIPT_DIR%
